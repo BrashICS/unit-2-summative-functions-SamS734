f0929dd10ec49e0aea89ac59e7820327fed7cf4f
Sam <samuel.sum@stu.ocsb.ca>
2024-10-21 11:14:34 -0400

    finished javascript portion of part 1

--- main.js
+++ main.js
@@ -14,6 +14,25 @@ document.getElementById("vertex").addEventListener("click", vertex);
 
 /*** Functions ***/
 
+/** PART 1 **/
+
+/* JavaScript */
+
+// Return the difference of two numbers
+function delta(a, b) {
+    return a - b;
+}
+
+// Return the slope of a line
+function slope(x1, y1, x2, y2) {
+    return delta(y2, y1) / delta (x2, x1);
+}
+
+// Return the average of two numbers
+function average(n1, n2) {
+    return (n1 + n2) / 2
+}
+
 // Round to the nearest `decimals` number of decimals
 function round(value, decimals) {
     return Math.round(value * 10**decimals) / 10**decimals;
@@ -21,10 +40,22 @@ function round(value, decimals) {
 
 // Round to the user's number of decimals
 function round_user(value) {
-    // Get the number of decimals from the "rounding" box
-    let d = Number(document.getElementById("rounding").value)
-
+// Get the number of decimals from the "rounding" box
+    let decimals = Number(document.getElementById("rounding").value);
+// Round the value to the user's request number of decimals and return it
+    return round(value, decimals);
 }
+
+// Length of a line segment
+function length(x1, y1, x2, y2) {
+    return Math.sqrt((delta(x2, x1))**2 + (delta (y2, y1))**2);
+}
+
+/* HTML */
+
+
+
+/** PART 2 **/
 
 // Calculate the y-value of a parabola from standand form
 function y_quad(a, b, c, x) {
